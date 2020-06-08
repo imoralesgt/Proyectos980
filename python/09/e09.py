782db47965c89dcee61ba065110d256387a29803
imoralesgt <ivan.rene.morales@gmail.com>
2020-06-08 13:08:22 -0600

    Ejemplo manejo de archivos separados por coma

---
+++ python/09/e09.py
@@ -0,0 +1,18 @@
+LISTADO = 'listado.csv'
+
+datos = []
+
+archivo = open(LISTADO, 'r')
+for linea in archivo:
+    registro = linea.split(',')
+    
+    #registro[-1] = registro[-1].split(' \n')[0]
+    registro[-1] = registro[-1].replace(' \n', '')  
+
+
+    datos.append(registro)
+
+archivo.close()
+
+for i in datos:
+    print(i)
