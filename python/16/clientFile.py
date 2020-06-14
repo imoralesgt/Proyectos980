2035268523131ed1e2df4b12ccd1313aa578331b
imoralesgt <ivan.rene.morales@gmail.com>
2020-06-14 16:16:19 -0600

    Socket TCP para envio de archivos

---
+++ python/16/clientFile.py
@@ -0,0 +1,25 @@
+import socket
+
+SERVER_ADDR = '157.245.82.242'
+SERVER_PORT = 980
+
+BUFFER_SIZE = 8 * 1024
+
+sock = socket.socket()
+sock.connect((SERVER_ADDR, SERVER_PORT))
+
+
+try:
+    buff = sock.recv(BUFFER_SIZE)
+    archivo = open('recibido', 'wb') #Aca se guarda el archivo entrante
+    while buff:
+        buff = sock.recv(BUFFER_SIZE) #Los bloques se van agregando al archivo
+        archivo.write(buff)
+
+    archivo.close() #Se cierra el archivo
+
+    print("Recepcion de archivo finalizada")
+
+finally:
+    print('Conexion al servidor finalizada')
+    sock.close() #Se cierra el socket
---
+++ python/16/serverFile.py
@@ -0,0 +1,27 @@
+#socket.sendfile() disponible desde Python 3.3
+
+import socket
+
+SERVER_ADDR = ''
+SERVER_PORT = 980
+
+
+BUFFER_SIZE = 8 * 1024 #8 KB para buffer de transferencia de archivos
+
+server_socket = socket.socket()
+server_socket.bind((SERVER_ADDR, SERVER_PORT))
+server_socket.listen(10) #1 conexion activa y 9 en cola
+try:
+    while True:
+        print("\nEsperando conexion remota...\n")
+        conn, addr = server_socket.accept()
+        print('Conexion establecida desde ', addr)
+        print('Enviando archivo de prueba de 5MB...')
+        with open('file5M', 'rb') as f: #Se abre el archivo a enviar en BINARIO
+            conn.sendfile(f, 0)
+            f.close()
+        conn.close()
+        print("\n\nArchivo enviado a: ", addr)
+finally:
+    print("Cerrando el servidor...")
+    server_socket.close()
