f42cb73a5c705aeb4f03c8d6387b15faf44e1499
imoralesgt <ivan.rene.morales@gmail.com>
2020-06-14 15:11:57 -0600

    Ejemplo socket UDP cliente servidor

---
+++ python/15/clientUDP.py
@@ -0,0 +1,36 @@
+import socket
+
+SERVER_ADDRESS = '157.245.82.242'
+SERVER_PORT = 980
+
+#BUFFER_SIZE = 16 #Si BUFFER_SIZE es menor al mensaje, este se corta
+BUFFER_SIZE = 4096 #Usamos buffer de 4KB
+
+
+
+# Se crea el socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Tupla de config. del servidor a quien se envia (y recibe de) la info
+serverAddress = (SERVER_ADDRESS, SERVER_PORT)
+message = 'Este mensaje se transmite del cliente al servidor. \
+ Se espera sea devuelto de regreso: UDP no es un protocolo confiable, \
+ pero para aplicaciones que toleran paquetes perdidos, funciona bien.'
+
+message = message.encode() #Se convierte de str a bytes
+
+
+try:
+
+    # Se manda la data directamente (sin realizar una conexión directa al server)
+    print('\nEnviando:  {!r}'.format(message))
+    sent = sock.sendto(message, serverAddress)
+
+    # Se espera respuesta de vuelta
+    print('\nEsperando recepcion de vuelta...\n')
+    data, server = sock.recvfrom(BUFFER_SIZE)
+    print('\nRecibido: {!r}\n\n'.format(data))
+
+finally:
+    print('Cerrando socket')
+    sock.close()
---
+++ python/15/serverUDP.py
@@ -0,0 +1,37 @@
+import socket
+
+IP_ADDRESS = '157.245.82.242'
+IP_ADDRESS_ALL = ''
+IP_PORT = 980
+
+#Si el buffer es menor al mensaje a enviar, la info llega incompleta
+BUFFER_SIZE = 4096 #Debe ser menor al tamaño máximo del datagrama de IPv4 (64 KB)
+
+
+# Se crea el socket UDP (SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Se levanta el servidor, y automáticamente comienza la escucha
+# NO HAY CONEXIÓN DIRECTA CON NINGÚN CLIENTE
+serverAddress = (IP_ADDRESS_ALL, IP_PORT)
+print('Levantando servidor en {},  puerto {} \n\n'.format(*serverAddress))
+try:
+
+    sock.bind(serverAddress)
+
+    while True:
+        print('\nEsperando cualquier mensaje, de cualquier cliente...')
+        data, address = sock.recvfrom(BUFFER_SIZE)
+
+        print('\nRecibidos {} bytes desde {}'.format(
+            len(data), address))
+        print(data)
+
+        if data:
+            sent = sock.sendto(data, address)
+            print('Enviados de vuelta {} bytes a {}'.format(
+                sent, address))
+
+finally:
+    print("Cerrando servidor...")
+    sock.close()
