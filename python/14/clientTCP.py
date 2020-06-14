5084ed1d642fd523949d1992fdbf37998fb9b017
imoralesgt <ivan.rene.morales@gmail.com>
2020-06-14 13:22:02 -0600

    Ejemplos Cliente y Servidor con Socket TCP

---
+++ python/14/clientTCP.py
@@ -0,0 +1,36 @@
+import socket
+
+SERVER_IP   = '157.245.82.242'
+SERVER_PORT = 980
+BUFFER_SIZE = 16
+
+# Se crea socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Se conecta al puerto donde el servidor se encuentra a la escucha
+server_address = (SERVER_IP, SERVER_PORT)
+print('Conectando a {} en el puerto {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+
+    # Se envia un texto codificado EN BINARIO
+    message = b'Este es un mensaje.  El texto se divide en bloques de BUFFER_SIZE bytes.'
+    print('\n\nEnviando el siguiente texto:  {!s}'.format(message))
+    sock.sendall(message) #Se envia utilizando "socket.sendall" 
+
+    print("\n\n")
+
+    # Esperamos la respuesta del ping servidor
+    bytesRecibidos = 0
+    bytesEsperados = len(message)
+
+    #TCP envia por bloques de BUFFER_SIZE bytes
+    while bytesRecibidos < bytesEsperados:
+        data = sock.recv(BUFFER_SIZE)
+        bytesRecibidos += len(data)
+        print('Recibido: {!s}'.format(data))
+
+finally:
+    print('\n\nConexion finalizada con el servidor')
+    sock.close()
---
+++ python/14/serverTCP.py
@@ -0,0 +1,41 @@
+import socket
+
+# Crea un socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+IP_ADDR = '192.168.1.100' #La IP donde desea levantarse el server
+IP_ADDR_ALL = '' #En caso que se quiera escuchar en todas las interfaces de red
+IP_PORT = 980 #Puerto al que deben conectarse los clientes
+
+BUFFER_SIZE = 16 #Bloques de 16 bytes
+
+# Bind the socket to the port
+serverAddress = (IP_ADDR_ALL, IP_PORT) #Escucha en todas las interfaces
+print('Iniciando servidor en {}, puerto {}'.format(*serverAddress))
+sock.bind(serverAddress) #Levanta servidor con parametros especificados
+
+# Habilita la escucha del servidor en las interfaces configuradas
+sock.listen(1) #El argumento indica el nivel de log a utilizar
+
+while True:
+    # Esperando conexion
+    print('Esperando conexion remota')
+    connection, clientAddress = sock.accept()
+    try:
+        print('connection from', clientAddress)
+
+        # Se envia informacion en bloques de BUFFER_SIZE bytes
+        # y se espera respuesta de vuelta
+        while True:
+            data = connection.recv(BUFFER_SIZE)
+            print('Recibido: {!r}'.format(data))
+            if data: #Si se reciben datos (o sea, no ha finalizado la transmision del cliente)
+                print('Enviando data de vuelta al cliente')
+                connection.sendall(data)
+            else:
+                print('Transmision finalizada desde el cliente ', clientAddress)
+                break
+
+    finally:
+        # Se baja el servidor para dejar libre el puerto para otras aplicaciones o instancias de la aplicacion
+        connection.close()
