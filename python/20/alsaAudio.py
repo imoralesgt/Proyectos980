bd88aef4488d5b22f2d80f8db90e5e81a7f6fa39
imoralesgt-ing <2281784440101@ingenieria.usac.edu.gt>
2020-06-16 18:47:09 +0000

    Audio con ALSA

---
+++ python/20/alsaAudio.py
@@ -0,0 +1,16 @@
+import os 
+import logging
+
+logging.basicConfig(
+    level = logging.DEBUG, 
+    format = '%(message)s'
+    )
+
+
+logging.info('Comenzando grabacion')
+os.system('arecord -d 10 -f U8 -r 8000 prueba.mp3')
+
+logging.info('Grabacion finalizada, inicia reproduccion')
+os.system('aplay prueba.mp3')
+
+
